2603f4cf4a30d30ec1c863b88b1c01fac404812d
IvaBabic <ivanabakosbabic@gmail.com>
2022-03-16 21:37:56 +0100

    create post page done

--- create-post.php
+++ create-post.php
@@ -1,3 +1,5 @@
+<?php include_once('db.php') ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -15,13 +17,78 @@
 
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 </head>
 
 <body>
+<?php
+  include('header.php');
+
+  if(isset($_POST['submit'])) {
+    $body = $_POST['body'];
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $currentDate = date("Y-m-d h:i");
+
+    if(empty($body) || empty($title) || empty($author)) {
+      echo("Nesto nije popunjeno");
+      return;
+    } else {
+      $sql = "INSERT INTO posts 
+      (title, body, author, created_at)
+      VALUES ('$title', '$body', '$author', '$currentDate');";
+    
+      $statement = $connection->prepare($sql);
+      $statement->execute();
+     header('Location: posts.php');
+
+    };
+  }
+
+?>
+
+  
+
+<main role="main" class="container">
+
+<div class="row">
+
+    <div class="col-sm-8 blog-main">
+
+        <div class="blog-post">
+  
+
+  <div>
+    <form style="color: #b34848" action="create-post.php" method="POST" id="postsForm" class="test">
+      <label for="fname">Title</label>
+      <input type="text" id="title" name="title" placeholder="Title" id="titlePosts">  <br>       
+      <label for="fname">Name</label>
+      <input type="text" name="author" placeholder="author" id="titlePosts"> <br>     
+      <label for="fname">Body</label>
+      <textarea name="body" placeholder ="Enter Post" rows = "10" id="bodyPosts"></textarea><br>
+
+    
+      <button class="ws-btn w3-block w3-margin-top w3-padding-16" type="submit" name="submit">Submit</button>
+
+    
+    </form>
+  </div>
+
+  </div>
+  </div>
+
+  <?php
+  include('sidebar.php');
+  ?>
+
+</div>
+
+</main>
+
 
 <?php
-    include('header.php');
-    include('sidebar.php')
+   
+    
     include('footer.php');
 ?>  
 
